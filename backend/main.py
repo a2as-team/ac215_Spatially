44b342f0b214bc0816abae49140a10f00647194b
Hun Kim <hunkim98@gmail.com>
2025-09-15 19:42:31 -0400

    Initialize

---
+++ backend/main.py
@@ -0,0 +1,2 @@
+if __name__ == "__main__":
+    print("Hello from ac215-spatially!")
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from ac215-spatially!")
+
+
+if __name__ == "__main__":
+    main()

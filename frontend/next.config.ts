496a435d8984c14470a946bdb25d19e2a574e654
Hun Kim <hunkim98@gmail.com>
2025-11-27 03:09:35 -0500

    Update for env

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   // Disabled in development to prevent map double-mount issues
   reactStrictMode: false,
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    SERVER_API_BASE_URL: process.env.SERVER_API_BASE_URL,
+  },
 };
 
 export default nextConfig;
